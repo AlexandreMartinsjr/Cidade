80f6a9fd4f4f2a599b13939fba014837fe11ae6f
AlexandreMartinsjr <159536757+AlexandreMartinsjr@users.noreply.github.com>
2024-07-31 10:52:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = 'imagem 1.png'
+    legendas.innerHTML= 'praia do farol'
+
+}
+function imagem2(){
+
+    lamp. src = 'imagem 2.png'
+    legendas.innerHTML= 'Centro da cidade'
+
+}
+
+function imagem3(){
+
+    lamp.src = 'imagem 3.png'
+    legendas.innerHTML= 'Escadão de cabo frio'
+}
+function imagem4(){
+
+    lamp. src = 'imagem 4.png'
+    legendas.innerHTML= 'Prainha'
+}
